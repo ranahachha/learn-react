0e8dffda5202fc47c6123c06e23d3bf8e1ab864e
Neekhil Rai <linkinrai@gmail.com>
2024-09-14 22:50:16 +0200

    create first react component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Profile from "./components/Profile";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1>Hello World</h1>;
+  return <Profile />;
 }
 
 export default App;
